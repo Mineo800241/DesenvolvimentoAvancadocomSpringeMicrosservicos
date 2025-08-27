bfa179cb2a6751e10c411e0dbe0cc7ee729d1d47
maxwell <maxmineopetro@gmail.com>
2025-08-27 14:59:54 -0300

    Initial Commit for this project

---
+++ src/main/java/br/edu/infnet/maxwellapi/model/repository/CompetidorRepository.java
@@ -0,0 +1,12 @@
+package br.edu.infnet.maxwellapi.model.repository;
+
+
+import br.edu.infnet.maxwellapi.model.domain.Competidor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompetidorRepository extends JpaRepository<Competidor, Integer> {
+
+
+}
